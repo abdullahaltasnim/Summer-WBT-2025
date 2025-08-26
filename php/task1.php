ebba40e6922c92559000093b2df4e1ae755d3fc0
abdullahaltasnim <abdullahaltasneem13@gmail.com>
2025-08-26 12:51:46 +0600

    26-08-2025

---
+++ php/task1.php
@@ -0,0 +1,62 @@
+
+
+
+
+<?php
+//////////////////////////////////////// Task 1 ///////////////////////////////////////
+// $length = 5;
+// $width = 5;
+// $area = $length * $width;
+// $parimeter = 2 * ($length + $width);
+
+// echo "Length: " . $length . " <br>";
+// echo "Width: " . $width . " <br>";
+// echo "Area of Rectangle =  $area <br>";
+// echo "Perimeter of Rectangle = $parimeter <br>";
+
+
+//////////////////////////////////////// Task 2 ///////////////////////////////////////
+
+// $price = 20;
+// $total = $price * (15 / 100);
+// $totalvatprice = $total + $price;
+// echo "Total VAT added price is  = $totalvatprice <br>";
+
+
+
+///////////////////////////////////////// Task 3 /////////////////////////////////////// 
+
+// $x = 10;
+
+// if ($x % 2 == 0) {
+//     echo " this number is even $x. <br>";
+// } else {
+//     echo "this number is odd $x.";
+// }
+
+///////////////////////////////////////// Task 4 /////////////////////////////////////// 
+
+// $x = 10;
+// $y = 20;
+// $z = 15;
+
+// if ($x >= $y && $x >= $z) {
+//     echo "the largest number is $x";
+// } elseif ($y >= $x && $y >= $z) {
+//     echo "the largest number is $y";
+// } else {
+//     echo "the largest number is $z";
+// }
+
+///////////////////////////////////////// Task 5 /////////////////////////////////////// 
+
+
+// for ($x = 10; $x < 100; $x++) {
+//     if ($x % 2 == 1) {
+//         echo " $x";
+//     }
+// }
+
+
+
+///////////////////////////////////////// Task 6 /////////////////////////////////////// 
